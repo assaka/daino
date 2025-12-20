89ddb77a9691c41322d995fc697a9866f3c7fb03
Hamid Sprtags <hamidelabassi99@gmail.com>
2025-12-20 09:27:04 +0100

    Temporary allow during testing that demo store can run

--- src/pages/admin/Stores.jsx
+++ src/pages/admin/Stores.jsx
@@ -110,6 +110,10 @@ export default function Stores() {
     const store = stores.find(s => s.id === storeId);
 
     // TODO: Re-enable after testing - Demo stores should not be allowed to run
+    // If store is in demo status,
+    //    -show modal explaining they need to
+    // -delete demo data first
+
     // if (store?.status === 'demo') {
     //   setStoreForDemo(store);
     //   setShowDemoCannotRunModal(true);
